5f439337fa14d93cb29ddde32e528468fbb7f2f7
Joshua Gorman <90810000+endevii@users.noreply.github.com>
2023-11-27 14:32:46 -0500

    Added titles for selection on  compete page

--- frontend/src/components/Compete/Compete.jsx
+++ frontend/src/components/Compete/Compete.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import styles from './Compete.module.css';
+import styles from "./Compete.module.css";
 
-import AgainstAi from './AgainstAi';
-import AgainstPlayer from './AgainstPlayer';
+import AgainstAi from "./AgainstAi";
+import AgainstPlayer from "./AgainstPlayer";
 
 export default function Compete() {
   const [againstAi, setAgainstAi] = useState(true);
   const [showButtons, setShowButtons] = useState(true);
-  const [difficulty, setDifficulty] = useState('none'); // ['beginner', 'normal', 'insane']
-  const [boardType, setBoardType] = useState('none'); // ['bubble', 'insertion', 'selection', 'merge']
+  const [difficulty, setDifficulty] = useState("none"); // ['beginner', 'normal', 'insane']
+  const [boardType, setBoardType] = useState("none"); // ['bubble', 'insertion', 'selection', 'merge']
   const [showBoardType, setShowBoardType] = useState(false);
   const [showDifficulty, setShowDifficulty] = useState(false);
 
@@ -40,14 +40,14 @@ export default function Compete() {
     } else if (!showButtons && !showBoardType) {
       setShowDifficulty(false);
       setShowBoardType(true);
-      setBoardType('none');
+      setBoardType("none");
     } else if (showDifficulty) {
-      setDifficulty('none');
+      setDifficulty("none");
       setShowDifficulty(false);
       setShowBoardType(true);
     } else {
-      setBoardType('none');
-      setDifficulty('none');
+      setBoardType("none");
+      setDifficulty("none");
       setShowBoardType(false);
       setShowDifficulty(false);
       setShowButtons(true);
@@ -57,41 +57,104 @@ export default function Compete() {
   function handleLeave() {
     setInGame(false);
     setShowButtons(true);
-    setDifficulty('none');
-    setBoardType('none');
+    setDifficulty("none");
+    setBoardType("none");
     setShowBoardType(false);
     setShowDifficulty(false);
   }
-
+  // console.log(showButtons, showBoardType, showDifficulty);
   return (
     <div>
       <div className={styles.main}>
         <h1>Compete</h1>
-        {showButtons || showBoardType || showDifficulty ? null : difficulty === 'none' || boardType === 'none' ? null : againstAi ? <AgainstAi difficulty={difficulty} boardType={boardType} /> : <AgainstPlayer difficulty={difficulty} boardType={boardType} />}
+        {!showButtons || showBoardType || showDifficulty ? null : (
+          <h2>Select Opponent Type</h2>
+        )}
+        {showButtons || !showBoardType || showDifficulty ? null : (
+          <h2>Select Board Type</h2>
+        )}
+        {showButtons || showBoardType || !showDifficulty ? null : (
+          <h2>Select Difficulty</h2>
+        )}
+        {showButtons || showBoardType || showDifficulty ? null : difficulty ===
+            "none" || boardType === "none" ? null : againstAi ? (
+          <AgainstAi difficulty={difficulty} boardType={boardType} />
+        ) : (
+          <AgainstPlayer difficulty={difficulty} boardType={boardType} />
+        )}
         {!showButtons ? null : (
           <div className={styles.select_container}>
-            <button className={styles.select_btn} onClick={() => handleButtons(true)}>Against AI</button>
-            <button className={styles.select_btn} onClick={() => handleButtons(false)}>Against Player</button>
+            <button
+              className={styles.select_btn}
+              onClick={() => handleButtons(true)}
+            >
+              Against AI
+            </button>
+            <button
+              className={styles.select_btn}
+              onClick={() => handleButtons(false)}
+            >
+              Against Player
+            </button>
           </div>
         )}
         {showDifficulty ? (
           <div className={styles.select_container}>
-            <button className={styles.select_btn} onClick={() => handleDifficulty('beginner')}>Beginner</button>
-            <button className={styles.select_btn} onClick={() => handleDifficulty('normal')}>Normal</button>
-            <button className={styles.select_btn} onClick={() => handleDifficulty('insane')}>Insane</button>
+            <button
+              className={styles.select_btn}
+              onClick={() => handleDifficulty("beginner")}
+            >
+              Beginner
+            </button>
+            <button
+              className={styles.select_btn}
+              onClick={() => handleDifficulty("normal")}
+            >
+              Normal
+            </button>
+            <button
+              className={styles.select_btn}
+              onClick={() => handleDifficulty("insane")}
+            >
+              Insane
+            </button>
           </div>
         ) : null}
         {showBoardType ? (
           <div className={styles.select_container}>
-            <button className={styles.select_btn} onClick={() => handleBoardType('bubble')}>Bubble Sort</button>
-            <button className={styles.select_btn} onClick={() => handleBoardType('insertion')}>Insertion Sort</button>
-            <button className={styles.select_btn} onClick={() => handleBoardType('selection')}>Selection Sort</button>
-            <button className={styles.select_btn} onClick={() => handleBoardType('merge')}>Merge Sort</button>
+            <button
+              className={styles.select_btn}
+              onClick={() => handleBoardType("bubble")}
+            >
+              Bubble Sort
+            </button>
+            <button
+              className={styles.select_btn}
+              onClick={() => handleBoardType("insertion")}
+            >
+              Insertion Sort
+            </button>
+            <button
+              className={styles.select_btn}
+              onClick={() => handleBoardType("selection")}
+            >
+              Selection Sort
+            </button>
+            <button
+              className={styles.select_btn}
+              onClick={() => handleBoardType("merge")}
+            >
+              Merge Sort
+            </button>
           </div>
         ) : null}
-        {showButtons || inGame ? null : <button className={styles.back_btn} onClick={() => handleBack()}>Back</button>}
+        {showButtons || inGame ? null : (
+          <button className={styles.back_btn} onClick={() => handleBack()}>
+            Back
+          </button>
+        )}
         {/* {!inGame ? null : <button className={styles.back_btn} onClick={() => handleLeave()}>Leave</button>} */}
       </div>
     </div>
-  )
+  );
 }
